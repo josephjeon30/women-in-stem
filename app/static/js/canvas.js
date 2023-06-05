5f133ef022ceadbbc74588a65ab12f21c5cfc262
Henry Bach <henrypbach@gmail.com>
2023-06-05 09:19:56 -0400

    create_postit()

--- app/static/js/canvas.js
+++ app/static/js/canvas.js
@@ -1,17 +1,20 @@
 var canvas = document.getElementById("notepad");
 // console.log(canvas);
 var ctx = canvas.getContext("2d");
+ctx.fillStyle = "rgb(255,0,0)";
+ctx.fillRect(0, 0, canvas.getBoundingClientRect().width, canvas.getBoundingClientRect().height);
 
 var create_postit = function(e) {
   let coords = get_mouse_pos(e);
   console.log(coords);
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.fillRect(coords[0], coords[1], 10, 10);
 };
 
 function get_mouse_pos (e) {
   var rect_pos = canvas.getBoundingClientRect();
-  x_pos = e.clientX;
-  y_pos = e.clientY;
-  // console.log(x_pos, y_pos);
+  x_pos = e.clientX - rect_pos.left;
+  y_pos = e.clientY - rect_pos.top;
   return [x_pos, y_pos];
 }
 
